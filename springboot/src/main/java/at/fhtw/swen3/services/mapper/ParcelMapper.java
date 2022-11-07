dbf5a9860c7c14c35896bdde11009e305eab12b9
chiara <chiara.babiak@gmail.com>
2022-11-07 14:06:24 +0100

    mapper corrected

--- springboot/src/main/java/at/fhtw/swen3/services/mapper/ParcelMapper.java
+++ springboot/src/main/java/at/fhtw/swen3/services/mapper/ParcelMapper.java
@@ -1,12 +1,12 @@
 package at.fhtw.swen3.services.mapper;
 
 
-import at.fhtw.swen3.persistence.repositories.ParcelRepository;
+import at.fhtw.swen3.persistence.entity.ParcelEntity;
 import at.fhtw.swen3.services.dto.Parcel;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface ParcelMapper{
-    ParcelRepository mapToTarget(Parcel parcel);
-    Parcel mapToSource(ParcelRepository parcelDTO);
+    ParcelEntity mapToTarget(Parcel parcel);
+    Parcel mapToSource(ParcelEntity parcelDTO);
 }
