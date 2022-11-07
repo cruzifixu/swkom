fb88e9ec092443ccf680113e0b90f7f35aed6cea
cruzifixu <franchezcacruz09@gmail.com>
2022-11-07 14:25:35 +0100

    master

--- springboot/src/main/java/at/fhtw/swen3/services/AppMapper.java
+++
@@ -1,9 +0,0 @@
-package at.fhtw.swen3.services;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class AppMapper {
-    public static void main(String[] args) { SpringApplication.run(AppMapper.class, args); }
-}
