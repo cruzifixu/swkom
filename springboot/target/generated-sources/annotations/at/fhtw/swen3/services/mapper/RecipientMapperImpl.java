2a8fee2135fd63da80cc9e491bbc742f72d2bc3d
chiara <chiara.babiak@gmail.com>
2022-12-15 13:33:56 +0100

    weird push

--- springboot/target/generated-sources/annotations/at/fhtw/swen3/services/mapper/RecipientMapperImpl.java
+++ springboot/target/generated-sources/annotations/at/fhtw/swen3/services/mapper/RecipientMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-12-07T13:40:40+0100",
+    date = "2022-12-15T13:32:38+0100",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 public class RecipientMapperImpl implements RecipientMapper {
